ce565645a5de5dfc24eaf68fb4355204106bb611
Anna Pittman <annaepittman@gmail.com>
2022-01-14 13:26:33 -0800

    create delete route for flavors controller

--- PierreTreat/Controllers/FlavorsController.cs
+++ PierreTreat/Controllers/FlavorsController.cs
@@ -78,5 +78,21 @@ namespace PierreTreat.Controllers
       _db.SaveChanges();
       return RedirectToAction("Details", new { id = flavor.FlavorId });
     }
+
+    public ActionResult Delete(int id)
+    {
+    var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+    return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
