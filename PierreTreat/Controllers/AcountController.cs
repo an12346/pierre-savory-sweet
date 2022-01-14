e503d55bfc8b048dd31da7daafadd0d192f5a726
Anna Pittman <annaepittman@gmail.com>
2022-01-14 12:04:40 -0800

    create register route for AccountController

--- PierreTreat/Controllers/AcountController.cs
+++ PierreTreat/Controllers/AcountController.cs
@@ -1,16 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-using RecipeBook.Models;
+using PierreTreat.Models;
 using System.Threading.Tasks;
-using RecipeBook.ViewModels;
+using PierreTreat.ViewModels;
 
 namespace PierreTreat.Controllers
 {
+  public class AccountController : Controller
+  {
   private readonly PierreTreatContext _db;
   private readonly UserManager<ApplicationUser> _userManager;
   private readonly SignInManager<ApplicationUser> _signManager;
 
-  publicAccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierreTreatContext db)
+  public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierreTreatContext db)
   {
     _userManager = userManager;
     _signInManager = signInManager;
@@ -20,5 +22,26 @@ namespace PierreTreat.Controllers
   public ActionResult Index()
   {
     return View();
+  }
+
+  public IActionResult Register()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public async Task<ActionResult> Register (RegisterViewModel model)
+  {
+    var user = new ApplicationUser { UserName = model.Email };
+    IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+    if (result.Succeeded)
+    {
+      return RedirectToAction("Index");
+    }
+    else
+    {
+      return View();
+    }
+    }
   }
 }
