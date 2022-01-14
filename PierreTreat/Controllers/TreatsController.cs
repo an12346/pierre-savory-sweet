44f81cbccca4ef5e16b83e9b959c6247a0167646
Anna Pittman <annaepittman@gmail.com>
2022-01-14 13:30:30 -0800

    create AddFlavor route for FlavorsController

--- PierreTreat/Controllers/TreatsController.cs
+++ PierreTreat/Controllers/TreatsController.cs
@@ -95,11 +95,11 @@ namespace PierreTreat.Controllers
     }
 
     public ActionResult AddFlavor(int id)
-{
+    {
     var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
     ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorType");
     return View(thisTreat);
-}
+    }
 
   [HttpPost]
   public ActionResult AddFlavor(Treat treat, int FlavorId)
